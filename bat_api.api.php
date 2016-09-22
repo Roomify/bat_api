621ac6444bf9882f3300d3efcc710f3ac54161f3
Nicolò Caruso <nicolo@istos.it>
2016-09-22 12:23:03 +0200

    Add ‘hook_bat_api_events_index_calendar_alter’

---
+++ bat_api.api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+function hook_bat_api_units_index_calendar_alter(&$units) {
+  // No example.
+}
+
+function hook_bat_api_events_index_calendar_alter(&$events, $context) {
+  // No example.
+}
