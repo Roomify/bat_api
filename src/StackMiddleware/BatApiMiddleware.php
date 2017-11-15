5839b2621199cb6ee6a3ebf50b904b07d6a19cf2
Nicolò Caruso <nicolo@istos.it>
2017-11-15 15:19:59 +0100

    Workaround to fix “_format” issue

---
+++ src/StackMiddleware/BatApiMiddleware.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\bat_api\StackMiddleware\BatApiMiddleware.php
+ */
+
+namespace Drupal\bat_api\StackMiddleware;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * Provides a HTTP middleware.
+ */
+class BatApiMiddleware implements HttpKernelInterface {
+
+  /**
+   * The wrapped HTTP kernel.
+   *
+   * @var \Symfony\Component\HttpKernel\HttpKernelInterface
+   */
+  protected $httpKernel;
+
+  /**
+   * Constructs a BatApiMiddleware object.
+   *
+   * @param \Symfony\Component\HttpKernel\HttpKernelInterface $kernel
+   *   The decorated kernel.
+   * @param mixed $optional_argument
+   *   (optional) An optional argument.
+   */
+  public function __construct(HttpKernelInterface $http_kernel) {
+    $this->httpKernel = $http_kernel;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = TRUE) {
+    if ($format = $request->get('_format')) {
+      $request->setRequestFormat($format);
+    }
+
+    return $this->httpKernel->handle($request, $type, $catch);
+  }
+
+}
